50e6743f3dd2be4742d87350ebc1b4335f0066f3
Mike Kibbel <mkibbel@gmail.com>
2012-11-04 23:45:04 -0500

    basic setuptools configuration and example application skeleton

---
+++ setup.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python
+
+from setuptools import setup, find_packages
+import dojoforms
+
+setup(
+    name="dojoforms",
+    version=dojoforms.__version__,
+    description="Dojo forms and widgets for Django",
+    author="Mike Kibbel",
+    author_email="mkibbel@gmail.com",
+    license="BSD License",
+    platforms=["OS Independent"],
+    install_requires=[
+        "Django>=1.4"
+    ],
+    packages=find_packages(exclude=["example", "example.*"]),
+    include_package_data=True,
+    zip_safe=False,
+)
